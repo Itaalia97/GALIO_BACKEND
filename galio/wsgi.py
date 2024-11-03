bc178e9bf8cc5e7d95edd8c83a96f48e9ad4ab52
Itaalia97 <italia.ibinarriaga@gmail.com>
2024-11-02 18:04:44 -0600

    Update wsgi.py with correct paths for production

--- galio/wsgi.py
+++ galio/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Agrega la ruta del proyecto y del entorno virtual
+sys.path.append('/home/galio/GALIO_BACKEND')
+sys.path.append('/home/galio/.pyenv/versions/galio/lib/python3.8/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'galio.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
